a905c72a549865f0967733137b6bf9e382774922
lukangkang <1157219723@qq.com>
2017-09-04 13:50:08 +0800

    定义仓储基类接口和默认实现，定义仓储中的几个数据操作函数

--- Surpass-Server/Surpass.Entities/Interface/IRepositoryBase.cs
+++ Surpass-Server/Surpass.Entities/Interface/IRepositoryBase.cs
@@ -2,25 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using Surpass.Infrastructure.Database;
 
-namespace Surpass.Domain.Services.Interface
+namespace Surpass.Domain.Interface
 {
     /// <summary>
     /// 仓储的接口
     /// </summary>
     /// <typeparam name="TEntity">实体类型</typeparam>
     /// <typeparam name="TPrimaryKey">主键类型</typeparam>
-    public interface IRepository<TEntity, TPrimaryKey>
+    public interface IRepositoryBase<TEntity, TPrimaryKey>
         where TEntity : class, IEntity<TPrimaryKey>
     {
         /// <summary>
-        /// 查询实体
-        /// 受这些过滤器的影响: 查询过滤器
+        /// 根据主键获取实体
         /// </summary>
-        /// <returns></returns>
-        IQueryable<TEntity> Query();
+        TEntity Get(TPrimaryKey id);
 
         /// <summary>
         /// 获取符合条件的单个实体
@@ -29,6 +26,21 @@ namespace Surpass.Domain.Services.Interface
         /// <param name="predicate">条件</param>
         /// <returns></returns>
         TEntity Get(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// 根据条件获取实体列表
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        IList<TEntity> GetMany(Expression<Func<TEntity, bool>> predicate = null);
+
+        /// <summary>
+        /// 根据过滤函数获取实体列表
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="fetch"></param>
+        /// <returns></returns>
+        TResult GetFunc<TResult>(Func<IQueryable<TEntity>, TResult> fetch);
 
         /// <summary>
         /// 计算符合条件的实体数量
@@ -45,13 +57,6 @@ namespace Surpass.Domain.Services.Interface
         /// <param name="entity">实体</param>
         /// <param name="update">更新函数</param>
         void Save(ref TEntity entity, Action<TEntity> update);
-
-        /// <summary>
-        /// 删除实体
-        /// 受这些过滤器的影响: 操作过滤器
-        /// </summary>
-        /// <param name="entity">实体</param>
-        void Delete(TEntity entity);
 
         /// <summary>
         /// 批量保存实体
@@ -73,6 +78,20 @@ namespace Surpass.Domain.Services.Interface
             Expression<Func<TEntity, bool>> predicate, Action<TEntity> update);
 
         /// <summary>
+        /// 删除实体
+        /// 受这些过滤器的影响: 操作过滤器
+        /// </summary>
+        /// <param name="entity">实体</param>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// 根据主键删除实体
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool Delete(TPrimaryKey id);
+
+        /// <summary>
         /// 批量删除实体
         /// 受这些过滤器的影响: 查询过滤器, 操作过滤器
         /// </summary>
@@ -81,5 +100,12 @@ namespace Surpass.Domain.Services.Interface
         /// <returns></returns>
         long BatchDelete(
             Expression<Func<TEntity, bool>> predicate, Action<TEntity> beforeDelete = null);
+
+        /// <summary>
+        /// 批量永久删除
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        long BatchDeleteForever(IEnumerable<TPrimaryKey> ids);
     }
 }
--- Surpass-Server/Surpass.Repository/RepositoryBase.cs
+++ Surpass-Server/Surpass.Repository/RepositoryBase.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.Extensions.DependencyInjection;
-using Surpass.Domain.Services.Interface;
+using Surpass.Domain.Filters;
+using Surpass.Domain.Interface;
 using Surpass.Domain.Uow.Interfaces;
 using Surpass.Infrastructure.Database;
 using Surpass.Domain.Uow.Extensions;
+using SurpassStandard.Utils;
 
 namespace Surpass.Repository
 {
-    public abstract class RepositoryBase<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey>
+    public abstract class RepositoryBase<TEntity, TPrimaryKey> : IRepositoryBase<TEntity, TPrimaryKey>
         where TEntity : class, IEntity<TPrimaryKey>
     {
         /// <summary>
@@ -18,26 +20,75 @@ namespace Surpass.Repository
         /// </summary>
         protected virtual IUnitOfWork UnitOfWork => Application.Ioc.GetService<IUnitOfWork>();
 
+        #region 查询方法
+
         /// <summary>
         /// 查询实体
         /// 受这些过滤器的影响: 查询过滤器
         /// </summary>
-        public virtual IQueryable<TEntity> Query()
+        private IQueryable<TEntity> Query()
         {
             var uow = UnitOfWork;
             var query = uow.Context.Query<TEntity>();
             return uow.WrapQuery<TEntity, TPrimaryKey>(query);
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// 根据主键获取实体
+        /// </summary>
+        public virtual TEntity Get(TPrimaryKey id)
+        {
+            var expr = ExpressionUtils.MakeMemberEqualiventExpression<TEntity>("Id", id);
+            using (UnitOfWork.Scope())
+            {
+                return Query().FirstOrDefault(expr);
+            }
+        }
+
+        /// <inheritdoc />
         /// <summary>
         /// 获取符合条件的单个实体
         /// 受这些过滤器的影响: 查询过滤器
         /// </summary>
         public virtual TEntity Get(Expression<Func<TEntity, bool>> predicate)
         {
-            return Query().FirstOrDefault(predicate);
+            using (UnitOfWork.Scope())
+            {
+                return Query().FirstOrDefault(predicate);
+            }
         }
 
+        /// <summary>
+        /// 根据条件获取实体列表
+        /// </summary>
+        public virtual IList<TEntity> GetMany(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            using (UnitOfWork.Scope())
+            {
+                var query = Query();
+                if (predicate != null)
+                {
+                    query = query.Where(predicate);
+                }
+                return query.ToList();
+            }
+        }
+
+        /// <summary>
+        /// 根据过滤函数获取实体列表
+        /// </summary>
+        public virtual TResult GetFunc<TResult>(
+            Func<IQueryable<TEntity>, TResult> fetch)
+        {
+            using (UnitOfWork.Scope())
+            {
+                return fetch(Query());
+            }
+        }
+
+
+        /// <inheritdoc />
         /// <summary>
         /// 计算符合条件的实体数量
         /// 受这些过滤器的影响: 查询过滤器
@@ -47,6 +98,11 @@ namespace Surpass.Repository
             return Query().LongCount(predicate);
         }
 
+        #endregion
+
+        #region 新增或修改方法
+
+        /// <inheritdoc />
         /// <summary>
         /// 添加或更新实体
         /// 受这些过滤器的影响: 操作过滤器
@@ -66,17 +122,7 @@ namespace Surpass.Repository
 
         }
 
-        /// <summary>
-        /// 删除实体
-        /// 受这些过滤器的影响: 操作过滤器
-        /// </summary>
-        public virtual void Delete(TEntity entity)
-        {
-            var uow = UnitOfWork;
-            uow.WrapBeforeDeleteMethod<TEntity, TPrimaryKey>(e => { })(entity);
-            uow.Context.Delete(entity);
-        }
-
+        /// <inheritdoc />
         /// <summary>
         /// 批量保存实体
         /// 受这些过滤器的影响: 操作过滤器
@@ -89,10 +135,11 @@ namespace Surpass.Repository
             uow.Context.BatchSave(ref entities, update);
         }
 
+        /// <inheritdoc />
         /// <summary>
-        /// 批量更新实体
+        /// 根据筛选条件批量更新实体
         /// 受这些过滤器的影响: 查询过滤器, 操作过滤器
-        /// </summary
+        /// </summary>
         public virtual long BatchUpdate(
             Expression<Func<TEntity, bool>> predicate, Action<TEntity> update)
         {
@@ -102,17 +149,61 @@ namespace Surpass.Repository
             return uow.Context.BatchUpdate(predicate, update);
         }
 
+        #endregion
+
+        #region 删除方法
+
+        /// <inheritdoc />
+        /// <summary>
+        /// 删除实体
+        /// 受这些过滤器的影响: 操作过滤器
+        /// </summary>
+        public virtual void Delete(TEntity entity)
+        {
+            var uow = UnitOfWork;
+            uow.WrapBeforeDeleteMethod<TEntity, TPrimaryKey>(e => { })(entity);
+            uow.Context.Delete(entity);
+        }
+
+        /// <summary>
+        /// 根据主键删除实体
+        /// </summary>
+        public virtual bool Delete(TPrimaryKey id)
+        {
+            var expr = ExpressionUtils.MakeMemberEqualiventExpression<TEntity>("Id", id);
+            using (UnitOfWork.Scope())
+            {
+                return BatchDelete(expr) > 0;
+            }
+        }
+
+        /// <inheritdoc />
         /// <summary>
         /// 批量删除实体
         /// 受这些过滤器的影响: 查询过滤器, 操作过滤器
         /// </summary>
         public virtual long BatchDelete(
-            Expression<Func<TEntity, bool>> predicate, Action<TEntity> beforeDelete)
+            Expression<Func<TEntity, bool>> predicate, Action<TEntity> beforeDelete = null)
         {
             var uow = UnitOfWork;
             predicate = uow.WrapPredicate<TEntity, TPrimaryKey>(predicate);
             beforeDelete = uow.WrapBeforeDeleteMethod<TEntity, TPrimaryKey>(beforeDelete);
             return uow.Context.BatchDelete(predicate, beforeDelete);
         }
+
+        /// <summary>
+        /// 批量永久删除
+        /// </summary>
+        public virtual long BatchDeleteForever(IEnumerable<TPrimaryKey> ids)
+        {
+            var uow = UnitOfWork;
+            using (uow.Scope())
+            using (uow.DisableQueryFilter(typeof(DeletedFilter)))
+            {
+                return BatchDelete(e => ids.Contains(e.Id));
+            }
+        }
+
+        #endregion
     }
 }
